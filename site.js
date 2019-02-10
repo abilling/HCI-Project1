38bb961bfc600dc149638334d69fdafd7e7f83cf
abilling <abilling@hawk.iit.edu>
2019-02-10 16:26:46 -0600

    Added some small amount of JS to check for working browser, also filled out some part of the phone form.

---
+++ site.js
@@ -0,0 +1,7 @@
+(function(){
+  // needed to see what kind of browser it is
+  if(!('querySelector' in document && 'addEventListener' in document)) {
+    //Javascript just wont show up.
+    return;
+  }
+}
