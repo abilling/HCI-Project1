3e9353a3bd472e1ee8c8731192694044eb6c1b96
abilling <abilling@hawk.iit.edu>
2019-02-10 18:37:55 -0600

    Everything has been linted, fixed if needed and passes validation.

--- site.js
+++ site.js
@@ -1,30 +1,31 @@
+"use strict";
+var tel_input = document.querySelector('#telephone');
+var submission= document.querySelector('#signup');
+var best_number = clean_number.replace(/^1/, '');
+var clean_number = this.value.replace(/\D/g, '');
+
 (function(){
   // needed to see what kind of browser it is
   if(!('querySelector' in document && 'addEventListener' in document)){
-    //Javascript just wont show up.
+    // Javascript just wont show up.
     return;
   }
   document.addEventListener('DOMContentLoaded', function(){
-    console.log('DOM has loaded.');
-    var heading_text = document.querySelector('#content h1').innerText;
-    console.log('the heading text is:', heading_text);
-    var submission= document.querySelector('#signup');
-    submission.setAttribute('disabled')
+    // console.log('DOM has loaded.');
+  // var heading_text = document.querySelector('#content h1').innerText;
+    // console.log('the heading text is:', heading_text);
+    submission.setAttribute('disabled');
   });
-  var tel_input = document.querySelector('#telephone');
   tel_input.addEventListener('focus', function(){
     console.log('Person has focused on the telephone input box');
   });
-  var tel_input = document.querySelector('#telephone');
   tel_input.addEventListener('blur', function(){
     console.log('Person has unfocused on the telephone input box');
   });
   tel_input.addEventListener('keyup', function(){
-   console.log('The value of #telephone is', this.value);
-   var clean_number = this.value.replace(/\D/g,'');
-   var best_number = clean_number.replace(/^1/,'');
-   if (best_number.length === 10) {
-     submission.removeAttribute('disabled');
-   }
- });
+    console.log('The value of #telephone is', this.value);
+    if (best_number.length === 10) {
+      submission.removeAttribute('disabled');
+    }
+  });
 }());
